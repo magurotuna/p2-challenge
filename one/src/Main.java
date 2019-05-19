bdab2084e3d42368c49ffe049912a768c6dfb5c4
Yusuke Tanaka <wing0920@gmail.com>
2019-05-19 23:26:19 +0900

    fix

--- one/src/Main.java
+++ one/src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Queue<Integer> q0 = new ImmutableQueue<>();
-        System.out.println(q0.isEmpty());
-        System.out.println(q0.head());
+        System.out.println(q0.isEmpty()); // true
+        System.out.println(q0.head()); // null
 
         Queue<Integer> q1 = q0.enQueue(99);
         Queue<Integer> q2 = q1.enQueue(1000);
-        System.out.println(q2.head());
+        System.out.println(q2.head()); // 99
 
         Queue<Integer> q3 = q2.deQueue().deQueue();
-        System.out.println(q3.isEmpty());
+        System.out.println(q3.isEmpty()); // true
 
         Queue<Integer> q4 = q3.enQueue(1).enQueue(2).enQueue(3).enQueue(4).enQueue(5).deQueue().deQueue();
-        System.out.println(q4.head());
+        System.out.println(q4.head()); // 3
     }
 }
